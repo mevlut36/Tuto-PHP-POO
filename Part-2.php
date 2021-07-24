7a0ff0a454b2d4f4a2fa82ad3195c329e6f4e68d
Mev <36679013+Hirss@users.noreply.github.com>
2021-07-24 02:47:51 +0200

    This time we will try with a form (PHP & HTML)

---
+++ Part-2.php
@@ -0,0 +1,22 @@
+<form method="post">
+    <input name="nom" autocomplete="off" placeholder="Nom"><br><br>
+    <input name="texte" autocomplete="off" placeholder="Texte">
+    <input type="submit">
+</form>
+
+<?php
+
+include("Article.php");
+if(isset($_POST["nom"], $_POST["texte"])){
+    $nom = $_POST["nom"];
+    $texte = $_POST["texte"];
+
+    $article = new Article($nom, $texte);
+    $article->getName("Je m'appelle ");
+    echo "<br>";
+    $article->getText("Voici mon texte : ");
+    echo "<br>";
+    $article->getDate("On est le : ");
+}
+
+?>
