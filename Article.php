0ac46a30996834546ccb0c6d06a1551dd903e6c9
Mev <36679013+Hirss@users.noreply.github.com>
2021-07-24 02:46:03 +0200

    Create Article.php

---
+++ Article.php
@@ -0,0 +1,27 @@
+<?php
+
+class Article {
+    private $name;
+    private $text;
+    private $date;
+
+    public function __construct($name, $text){
+        $this->name = $name;
+        $this->text = $text;
+    }
+
+    public function getName(string $txt){
+        echo $txt;
+        echo $this->name;
+    }
+
+    public function getText(string $txt){
+        echo $txt;
+        echo $this->text;
+    }
+
+    public function getDate(string $txt){
+        echo $txt;
+        echo "<b>" . date('Y-m-d / H:i') . "</b>";
+    }
+}
